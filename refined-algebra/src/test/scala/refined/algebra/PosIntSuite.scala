0d2377bc09d446ed07974b8ab567cff01f44e7f0
Arman Bilge <armanbilge@gmail.com>
2022-06-13 18:53:58 +0000

    Test for failing case

--- refined-algebra/src/test/scala/refined/algebra/PosIntSuite.scala
+++ refined-algebra/src/test/scala/refined/algebra/PosIntSuite.scala
@@ -4,13 +4,23 @@
 package refined.algebra
 
 import _root_.algebra.laws.RingLaws
+import algebra.ring.Ring
 import eu.timepit.refined.cats._
+import eu.timepit.refined.numeric.Positive
+import eu.timepit.refined.refineV
 import eu.timepit.refined.scalacheck.numeric._
 import eu.timepit.refined.types.numeric.PosInt
 import munit.DisciplineSuite
 
 class PosIntSuite extends DisciplineSuite {
 
-  checkAll("PosInt", RingLaws[PosInt].commutativeRing)
+  checkAll("PosInt", RingLaws[PosInt].multiplicativeSemigroup)
+
+  test("multiplicative associativity") {
+    val x = refineV[Positive](90973857).toOption.get
+    val y = refineV[Positive](1171327613).toOption.get
+    val z = refineV[Positive](926367060).toOption.get
+    assertEquals(Ring.times(Ring.times(x, y), z), Ring.times(x, Ring.times(y, z)))
+  }
 
 }
